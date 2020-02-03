a1f92436fea48a7635d8f65de2506fd33fd4c1b9
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2020-02-03 18:54:57 -0300

    First commit

---
+++ src/main/java/br/com/ferraz/improvemyself/ImproveMyselfApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferraz.improvemyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveMyselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveMyselfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferraz/improvemyself/ImproveMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferraz.improvemyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproveMyselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
